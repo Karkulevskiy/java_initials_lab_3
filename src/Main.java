2db5d196ee4cda26f15c786caf184446cd968536
Spacikl <mdkarkulevkiy@edu.hse.ru>
2024-03-30 07:46:45 +0300

    Добавил вывод пола человека, проверил на дату 29.01.2001

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
+    private static String[] endMan = {"евич", "ович"};
+    private  static String[] endWoman = {"евна", "овна"};
     public static void main(String[] args) {
-        System.out.println("Чтобы заврешить программу введите: exit");
+        System.out.println("Чтобы завершить программу введите: exit");
         System.out.println("Введите ФИО и дату рождения:");
         // Регекс выражение
         var regex = "[А-Яа-яЁё]+\\s[А-Яа-яЁё]+\\s[А-Яа-яЁё]+\\s(0[1-9]|[12][0-9]|3[01])\\.(0[1-9]|1[012])\\.(\\d{4})";
@@ -19,6 +21,8 @@ public class Main {
             if (data.matches(regex)) {
                 // Делим по пробелу и точкам
                 var splitData = data.split("[\\s.]");
+                // Будем по отчеству определять пол человека
+                var gender = getGender(splitData[2]);
                 // Получаем день, месяц и год
                 var days = Integer.parseInt(splitData[3]);
                 var month = Integer.parseInt(splitData[4]);
@@ -26,16 +30,14 @@ public class Main {
                 // Получаем текущую дату
                 var currDate = LocalDate.now();
                 // Вычитаем из текущей даты день, месяц и год, веденные пользователем
-                currDate = currDate.minusYears(year);
-                currDate = currDate.minusMonths(month);
-                currDate = currDate.minusDays(days);
+                currDate = currDate.minusYears(year).minusMonths(month).minusDays(days);
                 var totalYears = currDate.getYear();
                 // Если пользователь ввел дату, которая еще не наступила, то сообщим ему об этом
                 if (totalYears < 0) {
                     System.out.println("Человек еще не родился");
                 }
-                // Иначе выведем данные на консоль в формате Фам Имя Отч кол-во полных лет
-                else System.out.printf("Результат: %s %s %s %d\n", splitData[0], splitData[1], splitData[2], totalYears);
+                // Иначе выведем данные на консоль в формате Фам Имя Отч Пол кол-во полных лет
+                else System.out.printf("\n\nФИО: %s %s %s.\nПол: %s\nПолных лет: %d.\n\n", splitData[0], splitData[1], splitData[2], gender, totalYears);
             }
             // Иначе попросим ввести снова
             else {
@@ -44,5 +46,17 @@ public class Main {
             }
             System.out.println("Введите ФИО и дату рождения:");
         }
+    }
+    public static String getGender(String patronymic){
+        // Смотрим окончание отчества
+        for (var end : endMan){
+            if (patronymic.endsWith(end))
+                return "Мужской.";
+        }
+        for (var end : endWoman){
+            if (patronymic.endsWith(end))
+                return "Женский.";
+        }
+        return "Не определен!";
     }
 }
